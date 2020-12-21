747e234fe5787c4a0b1ce5bcb8caaedd0a905e56
maciejoleksy <maciejoleksy14@gmail.com>
2020-12-21 13:00:33 +0100

    update script

---
+++ script.js
@@ -0,0 +1,11 @@
+// elements
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".icon-weather");
+const temperatureElement = document.querySelector("temoerature-weather p");
+const descriptionElement = document.querySelector("description-weather p");
+const locationElement = document.querySelector("location-weather p");
+
+// data
+const weather = {
+
+};
